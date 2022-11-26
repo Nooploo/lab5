039be189d79b8ca4707daac3dc8a3d3d0c2dfa4a
Nooploo <bebeshyk@gmail.com>
2022-11-26 17:17:13 +0200

    r

---
+++ Printerr/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer printer = new Printer();
+            printer.Print("212121");
+
+            DELL1710 dELL1710 = new DELL1710();
+            dELL1710.Print("///");
+
+            Console.ReadLine();
+        }
+    }
+}
